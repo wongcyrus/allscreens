f40d021068e056f09fc1360a015ac3c1c1838d00
EC2 Default User <ec2-user@ip-172-31-89-137.ec2.internal>
2020-02-18 02:19:52 +0000

    Initial commit with Auth.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+
+import { withAuthenticator } from 'aws-amplify-react';
+import { Header } from 'semantic-ui-react';
+
+Amplify.configure(aws_exports);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header as="h1">
+      Hello World!
+    </Header>
   );
 }
 
-export default App;
+export default withAuthenticator(App, {
+  includeGreetings: true,
+  signUpConfig: {
+    hiddenDefaults: ['phone_number']
+  }
+});
+
