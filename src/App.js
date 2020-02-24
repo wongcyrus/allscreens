6dd05c8035422756f0bdf89ef053f7ddcd5f6623
Cyrus Wong <cywong@vtc.edu.hk>
2020-02-24 05:27:26 +0000

    Add usergroup logic and phone number.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css'
-import React from 'react';  
+import React, { Component } from 'react';
 
 import Amplify from 'aws-amplify';
+import { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 
 import { withAuthenticator } from 'aws-amplify-react';
@@ -12,22 +13,40 @@ import AllScreenView from './AllScreenView';
 
 Amplify.configure(aws_exports);
 
-function App() {
-  return (
-    <div>
-      <Header as="h1">
-        Share you computer screen and let work on the lab exercise toegther at home!
-      </Header>
-      <ScreenSharing></ScreenSharing>
-      <p>Don't start auto refresh if you want to save your bandwidth and computer resources!</p>
-      <AllScreenView></AllScreenView>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { isTeacher: false };
+  }
+
+  async componentDidMount() {
+    const user = await Auth.currentAuthenticatedUser();
+    const group = user.signInUserSession.accessToken.payload["cognito:groups"][0];
+    console.log(group);
+    this.setState({ isTeacher: "teachers" === group });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header as="h1">
+          Share you computer screen and let work on the lab exercise toegther at home!
+        </Header>
+        <ScreenSharing></ScreenSharing>
+        {this.state.isTeacher ? (
+          <div>
+            <p>Don't start auto refresh if you want to save your bandwidth and computer resources!</p>
+            <AllScreenView></AllScreenView>
+          </div>
+        ):""}
+      </div>
+    );
+  }
 }
 
 export default withAuthenticator(App, {
   includeGreetings: true,
   signUpConfig: {
-    hiddenDefaults: ['phone_number']
+    defaultCountryCode: "852"
   }
 });
