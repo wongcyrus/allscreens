64faacb6cd9e36d4f90afd1fa38f4cb5dbbc0630
Cyrus Wong <cywong@vtc.edu.hk>
2020-03-10 07:23:48 +0000

    Add Cloud Innovation Lab.

---
+++ amplify/backend/xr/VirtualTutor/sumerian_exports.js
@@ -0,0 +1,5 @@
+{
+  "url": "https://sumerian.us-east-1.amazonaws.com/20180801/projects/Drafts-assumed-role%2FCrossAccountSignin%2Fcyrus/release/authTokens?sceneId=6ee1bb051bfe4cd59c39a22f4affd16c.scene",
+  "sceneId": "6ee1bb051bfe4cd59c39a22f4affd16c.scene",
+  "region": "us-east-1"
+}
