1ec6a70176f7f66374c92c770bb8a5aa84342727
EC2 Default User <ec2-user@ip-172-31-36-213.ec2.internal>
2020-10-29 09:28:59 +0000

    Replace Virtual Tutor Slim.

--- amplify/backend/xr/VirtualTutor/sumerian_exports.js
+++
@@ -1,5 +0,0 @@
-{
-  "url": "https://sumerian.us-east-1.amazonaws.com/20180801/projects/Drafts-user%2Fcyrus/release/authTokens?sceneId=403d3609087647d983cfd588f214370f.scene",
-  "sceneId": "403d3609087647d983cfd588f214370f.scene",
-  "region": "us-east-1"
-}
